d9209d11243d366a01e5f6f7d8a66e2965dc92ea
Somendrat22 <somendrat22@gmail.com>
2025-01-19 12:10:51 +0530

    exp: initial code setup done

---
+++ src/test/java/com/bms/exp/api/bms_exp_api/BmsExpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bms.exp.api.bms_exp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsExpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
